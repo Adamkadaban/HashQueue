8f9e428ec46e8c0f3cd08af4b3e885dbccc12905
Adamkadaban <adamkadaban@gmail.com>
2023-12-01 22:42:36 -0500

    added crackPcap endpoint that allows for uploading files

--- main.py
+++ main.py
@@ -32,15 +32,21 @@ def load_cracked_hashes():
 
     return cracked_hashes
 
+def update_cracked_hashes(hash, result):
+    cracked_hashes[hash] = result
+
 def crack_hash(hash_to_crack):
     # Replace this command with your actual hashcat command
     # For example: subprocess.run(['hashcat', '-m', '0', hash_to_crack])
 	# TODO: Deal with unknown hash mode. 
     print(f"Cracking hash: {hash_to_crack}")
+    with open('/tmp/HashQueue.hash', 'w') as fout:
+        fout.write(hash_to_crack)
+    subprocess.run(['hashcat', '-m', '22000', '/tmp/HashQueue.hash', '/usr/share/wordlists/rockyou.txt'])
     return f"Hash cracked for {hash_to_crack}"
 
 
-@app.route('/crack', methods=['POST'])
+@app.route('/crackHash', methods=['POST'])
 def crack_hash_endpoint():
     data = request.get_json()
 
@@ -58,6 +64,34 @@ def crack_hash_endpoint():
     hash_queue.put(hash_to_crack)
 
     return jsonify({'result': 'Hash added to the cracking queue'})
+
+@app.route('/crackPcap', methods=['PUT'])
+def crack_pcap_endpoint():
+    if 'file' not in request.files:
+        return jsonify({'error': 'File not provided'}), 400
+
+    file = request.files['file']
+
+    if file.filename = '':
+        return jsonify({'error':'No file selected'}), 400
+
+    filename = secure_filename(file.filename)
+    filepath = os.path.join('/opt/HashQueue/data', filename)
+    file.save(path)
+
+    subprocess.run(['hcxpcapngtool', '-o', '/tmp/HashQueue.hash', filepath])
+
+    if os.path.exists('/tmp/HashQueue.hash'):
+        with open('/tmp/HashQueue.hash') as f:
+            raw_hashes = [h.rstrip() for h in f.readlines()]
+
+        for hash in raw_hashes:
+            crack_hash(hash)
+
+        return jsonify({'result': 'Hashes added to the cracking queue'}), 200
+    else:
+        return jsonify({'error': 'Hashes not found in pcap'}), 400
+
 
 def process_queue():
     while not hash_queue.empty():
